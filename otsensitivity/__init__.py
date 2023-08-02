b428cbc90d97dfb6504176ff04ff96f99bb68fc5
Michael BAUDIN <michael.baudin@gmail.com>
2023-08-02 12:32:51 +0200

    Format code with black

--- otsensitivity/__init__.py
+++ otsensitivity/__init__.py
@@ -1,10 +1,16 @@
 """otSensitivity module."""
 from .sobol import sobol_saltelli
 from .cosi import cosi
-from .visualization import (plot_indices, pairplot)
-from .moments import (cusunoro, ecdf, moment_independent)
+from .visualization import plot_indices, pairplot
+from .moments import cusunoro, ecdf, moment_independent
 
-__all__ = ['sobol_saltelli', 'cosi',
-           'cusunoro', 'ecdf', 'moment_independent',
-           'plot_indices', 'pairplot']
-__version__ = '1.0'
+__all__ = [
+    "sobol_saltelli",
+    "cosi",
+    "cusunoro",
+    "ecdf",
+    "moment_independent",
+    "plot_indices",
+    "pairplot",
+]
+__version__ = "1.0"
--- otsensitivity/visualization.py
+++ otsensitivity/visualization.py
@@ -24,15 +24,16 @@ def save_show(fname, figures, **kwargs):
     if fname is not None:
         pdf = matplotlib.backends.backend_pdf.PdfPages(fname)
         for fig in figures:
-            pdf.savefig(fig, transparent=True, bbox_inches='tight', **kwargs)
+            pdf.savefig(fig, transparent=True, bbox_inches="tight", **kwargs)
         pdf.close()
     else:
         plt.show()
-    plt.close('all')
+    plt.close("all")
 
 
-def plot_indices(indices, conf=None, plabels=None, polar=False, xdata=None,
-                 xlabel='x', fname=None):
+def plot_indices(
+    indices, conf=None, plabels=None, polar=False, xdata=None, xlabel="x", fname=None
+):
     """Plot Sensitivity indices.
 
     If `len(indices)>2` map indices are also plotted along with aggregated
@@ -59,9 +60,10 @@ def plot_indices(indices, conf=None, plabels=None, polar=False, xdata=None,
         conf = [None, None]
     p_len = len(indices[0])
     if plabels is None:
-        plabels = ['x' + str(i) for i in range(p_len)]
-    objects = [[r"$S_{" + p + r"}$", r"$S_{T_{" + p + r"}}$"]
-               for i, p in enumerate(plabels)]
+        plabels = ["x" + str(i) for i in range(p_len)]
+    objects = [
+        [r"$S_{" + p + r"}$", r"$S_{T_{" + p + r"}}$"] for i, p in enumerate(plabels)
+    ]
 
     s_lst = [item for sublist in objects for item in sublist]
     x_pos = np.arange(p_len)
@@ -76,61 +78,102 @@ def plot_indices(indices, conf=None, plabels=None, polar=False, xdata=None,
     if not polar:
         if len(indices) > 1:
             # Total orders
-            ax.bar(x_pos, indices[1] - indices[0], capsize=4, ecolor='g',
-                   error_kw={'elinewidth': 3, 'capthick': 3}, yerr=conf[1],
-                   align='center', alpha=0.5, color='c', bottom=indices[0],
-                   label='Total order')
+            ax.bar(
+                x_pos,
+                indices[1] - indices[0],
+                capsize=4,
+                ecolor="g",
+                error_kw={"elinewidth": 3, "capthick": 3},
+                yerr=conf[1],
+                align="center",
+                alpha=0.5,
+                color="c",
+                bottom=indices[0],
+                label="Total order",
+            )
 
         # First orders
-        ax.bar(x_pos, indices[0], capsize=3, yerr=conf[0], align='center',
-               alpha=0.5, color='r', label='First order')
+        ax.bar(
+            x_pos,
+            indices[0],
+            capsize=3,
+            yerr=conf[0],
+            align="center",
+            alpha=0.5,
+            color="r",
+            label="First order",
+        )
 
         ax.set_xticks(x_pos)
         ax.set_xticklabels(plabels)
-        ax.set_ylabel('Sensitivity indices')
-        ax.set_xlabel('Input parameters')
+        ax.set_ylabel("Sensitivity indices")
+        ax.set_xlabel("Input parameters")
     else:
 
-        def _polar_caps(theta, radius, ax, color='k', linewidth=1):
+        def _polar_caps(theta, radius, ax, color="k", linewidth=1):
             """Error bar caps in polar coordinates."""
             peri = np.pi * 1 / 180
             for th, _r in zip(theta, radius):
                 th_err = peri / _r
                 local_theta = np.linspace(-th_err / 2, th_err / 2, 10) + th
                 local_r = np.ones(10) * _r
-                ax.plot(local_theta, local_r, color=color, marker='',
-                        linewidth=linewidth, label=None)
+                ax.plot(
+                    local_theta,
+                    local_r,
+                    color=color,
+                    marker="",
+                    linewidth=linewidth,
+                    label=None,
+                )
             return ax
 
         theta = np.linspace(0.0, 2 * np.pi, p_len, endpoint=False)
 
-        ax.bar(theta, indices[0], width=2 * np.pi / p_len,
-               alpha=0.5, tick_label=plabels, color='r', label='First order')
+        ax.bar(
+            theta,
+            indices[0],
+            width=2 * np.pi / p_len,
+            alpha=0.5,
+            tick_label=plabels,
+            color="r",
+            label="First order",
+        )
 
         if len(indices) > 1:
-            ax.bar(theta, indices[1] - indices[0], width=2 * np.pi / p_len,
-                   alpha=0.5, color='c', bottom=indices[0], ecolor='g',
-                   label='Total order')
+            ax.bar(
+                theta,
+                indices[1] - indices[0],
+                width=2 * np.pi / p_len,
+                alpha=0.5,
+                color="c",
+                bottom=indices[0],
+                ecolor="g",
+                label="Total order",
+            )
 
         # Separators
         maxi = np.max([indices[0], indices[1]])
-        ax.plot([theta + np.pi / p_len, theta + np.pi / p_len],
-                [[0] * p_len, [maxi] * p_len], c='gray', label=None)
+        ax.plot(
+            [theta + np.pi / p_len, theta + np.pi / p_len],
+            [[0] * p_len, [maxi] * p_len],
+            c="gray",
+            label=None,
+        )
 
         if conf[0] is not None:
             # Total orders errors caps
-            _polar_caps(theta, indices[1] + conf[1], ax, color='g', linewidth=3)
-            _polar_caps(theta, indices[1] - conf[1], ax, color='g', linewidth=3)
+            _polar_caps(theta, indices[1] + conf[1], ax, color="g", linewidth=3)
+            _polar_caps(theta, indices[1] - conf[1], ax, color="g", linewidth=3)
             rad_ = np.array([indices[1] + conf[1], indices[1] - conf[1]])
             rad_[rad_ < 0] = 0
-            ax.plot([theta, theta], rad_, color='g', linewidth=3, label=None)
+            ax.plot([theta, theta], rad_, color="g", linewidth=3, label=None)
 
             # First orders errors caps
-            _polar_caps(theta, indices[0] + conf[0], ax, color='k')
-            _polar_caps(theta, indices[0] - conf[0], ax, color='k')
+            _polar_caps(theta, indices[0] + conf[0], ax, color="k")
+            _polar_caps(theta, indices[0] - conf[0], ax, color="k")
             rad_ = np.array([indices[0] + conf[0], indices[0] - conf[0]])
             rad_[rad_ < 0] = 0
-            ax.plot([theta, theta], rad_, color='k', label=None)
+            ax.plot([theta, theta], rad_, color="k", label=None)
 
         ax.set_rmin(0)
 
@@ -141,19 +184,19 @@ def plot_indices(indices, conf=None, plabels=None, polar=False, xdata=None,
         if xdata is None:
             xdata = np.linspace(0, 1, n_xdata)
 
-        fig = plt.figure('Sensitivity Map')
+        fig = plt.figure("Sensitivity Map")
         ax = fig.add_subplot(111)
         figs.append(fig)
         axs.append(ax)
 
         indices = np.hstack(indices[2:]).T
-        s_lst = np.array(objects).T.flatten('C').tolist()
+        s_lst = np.array(objects).T.flatten("C").tolist()
         for sobol, label in zip(indices, s_lst):
             ax.plot(xdata, sobol, linewidth=3, label=label)
         ax.set_xlabel(xlabel)
-        ax.set_ylabel('Sensitivity ')
+        ax.set_ylabel("Sensitivity ")
         ax.set_ylim(-0.1, 1.1)
-        ax.legend(bbox_to_anchor=(1.04, 1), loc='upper left')
+        ax.legend(bbox_to_anchor=(1.04, 1), loc="upper left")
 
     save_show(fname, figs)
 
@@ -177,14 +220,14 @@ def pairplot(sample, data, plabels=None, flabel=None, fname=None):
     n_dim = sample.shape[1]
 
     if plabels is None:
-        plabels = ['x' + str(i) for i in range(n_dim)]
+        plabels = ["x" + str(i) for i in range(n_dim)]
 
     fig, axs = plt.subplots(1, n_dim)
     for i in range(n_dim):
-        axs[i].scatter(sample[:, i], data, marker='+')
+        axs[i].scatter(sample[:, i], data, marker="+")
         axs[i].set_xlabel(plabels[i])
 
-    axs[0].set_ylabel('F' if flabel is None else flabel)
+    axs[0].set_ylabel("F" if flabel is None else flabel)
 
     save_show(fname, [fig])
 
